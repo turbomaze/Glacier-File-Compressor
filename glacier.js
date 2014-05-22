788bd4b5e110089147365eb3a64a7f2759450dd1
Anthony Liu <anthonyscottliu@gmail.com>
2014-05-21 23:15:36 -0700

    Replaces the n-grams in order of their abilities to be compressed.

--- glacier.js
+++ glacier.js
@@ -11,10 +11,14 @@ var fs = require('fs');
 
 /**********
  * config */
-var MAX_NUM_REPL = 3000;
+var MAX_NUM_REPL = 13750
 var SPLIT_CHAR = ' ';
 var IN_FILE = 'test.txt';
 var OUT_FILE = 'out.txt';
+
+/*************
+ * constants */
+var splitCharsPatn = new RegExp(SPLIT_CHAR, 'g');
 
 /*********************
  * working variables */
@@ -33,7 +37,7 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
         replacements.push(gen());
         gen = mb(mIncrString, gen);
     }
-    
+
     //count the unigrams
     var tokens = data.split(SPLIT_CHAR);
     var countsObj = {};
@@ -57,21 +61,31 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
         else countsObj[t] = 1;
     }
 
-    //load those counts into an array and sort them
-    var counts = [];
-    for (var token in countsObj) counts.push([token, countsObj[token]]);
-    counts.sort(function(a, b) { //take the length of the word into account
-        return b[0].length*(b[1]-1) - a[0].length*(a[1]-1);
+    //turn those counts into scores
+    var scores = [];
+    for (var token in countsObj) {
+        var score = token.length*countsObj[token]; //number of characters saved
+        score -= 6; //punctuation in the JSON
+        score -= token.length; //length of the token in the JSON
+        //the length of the replace string will be subtracted later
+        scores.push([token, score]);
+    }
+
+    //sort them
+    scores.sort(function(a, b) {
+        return b[1] - a[1]; //by their score
     });
 
     //map the most repeated words to the replacement strings
-    var encodeMap = {};
+    var encodeMap = [];
     var decodeMap = {};
     var idx = 0;
-    for (var ai = 0; ai < counts.length && ai < MAX_NUM_REPL; ai++) {
-        var t = counts[ai][0], repl = replacements[idx];
-        if (repl.length < t.length && counts[ai][1] > 1) {
-            encodeMap[t] = repl, decodeMap[repl] = t;
+    for (var ai = 0; ai < scores.length && ai < MAX_NUM_REPL; ai++) {
+        var t = scores[ai][0], repl = replacements[idx];
+        var adjScore = scores[ai][1] - countsObj[t]*repl.length;
+        if (adjScore > 0) {
+            encodeMap.push([t, repl]); //an array because order matters
+            decodeMap[repl] = t;
             idx++;
         }
     }
@@ -79,11 +93,31 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
     //only include necessary mappings
     var smallDecodeMap = {};
 
+    //step through the replacements
+    for (var ai = 0; ai < encodeMap.length; ai++) {
+        var search = encodeMap[ai][0].match(splitCharsPatn);
+        var ngram = search ? search.length : 0;
+        var tmp = [];
+        for (var bi = 0; bi < tokens.length-ngram; bi++) {
+            var t = tokens[bi];
+            for (var ti = 1; ti <= ngram; ti++) t += SPLIT_CHAR+tokens[bi+ti];
+            if (t === encodeMap[ai][0]) {
+                tmp.push(encodeMap[ai][1]);
+                if (!smallDecodeMap.hasOwnProperty(encodeMap[ai][1])) {
+                    smallDecodeMap[encodeMap[ai][1]] = t;
+                }
+                bi += ngram;
+            } else tmp.push(tokens[bi]);
+        }
+        tokens = tmp;
+    }
+
+/*
     //replace the trigrams
     var trigramTmp = [];
     for (var ai = 0; ai < tokens.length-2; ai++) {
         var t = tokens[ai]+SPLIT_CHAR+tokens[ai+1]+SPLIT_CHAR+tokens[ai+2];
-        if (encodeMap.hasOwnProperty(t) && encodeMap[t].length < t.length) {
+        if (encodeMap.hasOwnProperty(t)) {
             trigramTmp.push(encodeMap[t]);
             if (!smallDecodeMap.hasOwnProperty(encodeMap[t])) {
                 smallDecodeMap[encodeMap[t]] = t;
@@ -97,7 +131,7 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
     var bigramTmp = [];
     for (var ai = 0; ai < tokens.length-1; ai++) {
         var t = tokens[ai]+SPLIT_CHAR+tokens[ai+1];
-        if (encodeMap.hasOwnProperty(t) && encodeMap[t].length < t.length) {
+        if (encodeMap.hasOwnProperty(t)) {
             bigramTmp.push(encodeMap[t]);
             if (!smallDecodeMap.hasOwnProperty(encodeMap[t])) {
                 smallDecodeMap[encodeMap[t]] = t;
@@ -117,6 +151,7 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
             }
         }
     }
+*/
     
     //assemble the output
     var ret = JSON.stringify(smallDecodeMap)+'\n'+tokens.join(SPLIT_CHAR);
