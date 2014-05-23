2849330211f61086968093ba5fa627b88c5affde
Anthony Liu <anthonyscottliu@gmail.com>
2014-05-22 17:28:51 -0700

    Prunes away mappings that have no chance of being used, speeding up the program considerably.

--- glacier.js
+++ glacier.js
@@ -11,7 +11,8 @@ var fs = require('fs');
 
 /**********
  * config */
-var MAX_NUM_REPL = 13750
+var MAX_NUM_REPL = 14000;
+var MAX_N_GRAM = 2;
 var SPLIT_CHAR = ' ';
 var IN_FILE = 'test.txt';
 var OUT_FILE = 'out.txt';
@@ -38,27 +39,16 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
         gen = mb(mIncrString, gen);
     }
 
-    //count the unigrams
+    //count the ngrams
     var tokens = data.split(SPLIT_CHAR);
     var countsObj = {};
-    for (var ai = 0; ai < tokens.length; ai++) {
-        var t = tokens[ai];
-        if (countsObj.hasOwnProperty(t)) countsObj[t] += 1;
-        else countsObj[t] = 1;
-    }
-
-    //count the bigrams
-    for (var ai = 0; ai < tokens.length-1; ai++) {
-        var t = tokens[ai]+SPLIT_CHAR+tokens[ai+1];
-        if (countsObj.hasOwnProperty(t)) countsObj[t] += 1;
-        else countsObj[t] = 1;
-    }
-
-    //count the trigrams
-    for (var ai = 0; ai < tokens.length-2; ai++) {
-        var t = tokens[ai]+SPLIT_CHAR+tokens[ai+1]+SPLIT_CHAR+tokens[ai+2];
-        if (countsObj.hasOwnProperty(t)) countsObj[t] += 1;
-        else countsObj[t] = 1;
+    for (var ni = 0; ni < MAX_N_GRAM; ni++) {
+        for (var ai = 0; ai < tokens.length; ai++) {
+            var t = tokens[ai];
+            for (var ti = 1; ti <= ni; ti++) t += SPLIT_CHAR+tokens[ai+ti];
+            if (countsObj.hasOwnProperty(t)) countsObj[t] += 1;
+            else countsObj[t] = 1;
+        }
     }
 
     //turn those counts into scores
@@ -71,91 +61,90 @@ fs.readFile(IN_FILE, 'ascii', function(err, data) {
         scores.push([token, score]);
     }
 
-    //sort them
+    //sort the ngrams
     scores.sort(function(a, b) {
         return b[1] - a[1]; //by their score
     });
 
     //map the most repeated words to the replacement strings
     var encodeMap = [];
+    var idxsOfEncodings = {};
     var decodeMap = {};
     var idx = 0;
-    for (var ai = 0; ai < scores.length && ai < MAX_NUM_REPL; ai++) {
+    for (var ai = 0; ai < scores.length && idx < MAX_NUM_REPL; ai++) {
         var t = scores[ai][0], repl = replacements[idx];
         var adjScore = scores[ai][1] - countsObj[t]*repl.length;
         if (adjScore > 0) {
+            //enables quick access to each replacement's index later on
+            idxsOfEncodings[t] = encodeMap.length-1;
             encodeMap.push([t, repl]); //an array because order matters
             decodeMap[repl] = t;
             idx++;
         }
     }
 
+    //remove unreachable mappings (bi/tri grams preceded by unigrams)
+    var smallEncMap = [];
+    for (var ai = 0; ai < encodeMap.length; ai++) {
+        var parts = encodeMap[ai][0].split(SPLIT_CHAR);
+        if (parts.length > 1) { //bigram or above
+            var reachable = true;
+            //for each feasible sub-ngrams size
+            for (var ni = 1; ni < parts.length; ni++) {
+                //iterate through the possible ngrams
+                for (var bi = 0; bi < parts.length-ni+1; bi++) {
+                    var subngram = parts[bi];
+                    for (var ti = 1; ti < ni; ti++) {
+                        t += SPLIT_CHAR+parts[bi+ti];
+                    }
+                    //if this encoding is broken up by a sub-ngram
+                    if (idxsOfEncodings.hasOwnProperty(subngram) &&
+                        idxsOfEncodings[subngram] < ai) {
+                        reachable = false; //then it isn't reachable!
+                        break;
+                    }
+                }
+                if (!reachable) break;
+            }
+            if (reachable) smallEncMap.push(encodeMap[ai]);
+        } else {
+            smallEncMap.push(encodeMap[ai]);
+        }
+    }
+    console.log(
+        'Before pruning: '+encodeMap.length +
+        '; After: '+smallEncMap.length
+    );
+
     //only include necessary mappings
-    var smallDecodeMap = {};
+    var smallDecMap = {};
 
     //step through the replacements
-    for (var ai = 0; ai < encodeMap.length; ai++) {
-        var search = encodeMap[ai][0].match(splitCharsPatn);
+    for (var ai = 0; ai < smallEncMap.length; ai++) {
+        var search = smallEncMap[ai][0].match(splitCharsPatn);
         var ngram = search ? search.length : 0;
         var tmp = [];
         for (var bi = 0; bi < tokens.length-ngram; bi++) {
             var t = tokens[bi];
-            for (var ti = 1; ti <= ngram; ti++) t += SPLIT_CHAR+tokens[bi+ti];
-            if (t === encodeMap[ai][0]) {
-                tmp.push(encodeMap[ai][1]);
-                if (!smallDecodeMap.hasOwnProperty(encodeMap[ai][1])) {
-                    smallDecodeMap[encodeMap[ai][1]] = t;
+            for (var ti = 1; ti <= ngram; ti++) {
+                t += SPLIT_CHAR+tokens[bi+ti];
+            }
+            if (t === smallEncMap[ai][0]) {
+                tmp.push(smallEncMap[ai][1]);
+                if (!smallDecMap.hasOwnProperty(smallEncMap[ai][1])) {
+                    smallDecMap[smallEncMap[ai][1]] = t;
                 }
                 bi += ngram;
             } else tmp.push(tokens[bi]);
         }
+        //larger ngrams cut off the last few tokens, so add them
+        tmp.push.apply(tmp, tokens.splice(tokens.length-ngram, ngram));
         tokens = tmp;
     }
 
-/*
-    //replace the trigrams
-    var trigramTmp = [];
-    for (var ai = 0; ai < tokens.length-2; ai++) {
-        var t = tokens[ai]+SPLIT_CHAR+tokens[ai+1]+SPLIT_CHAR+tokens[ai+2];
-        if (encodeMap.hasOwnProperty(t)) {
-            trigramTmp.push(encodeMap[t]);
-            if (!smallDecodeMap.hasOwnProperty(encodeMap[t])) {
-                smallDecodeMap[encodeMap[t]] = t;
-            }
-            ai += 2;
-        } else trigramTmp.push(tokens[ai]);
-    }
-    tokens = trigramTmp;
-
-    //replace the bigrams
-    var bigramTmp = [];
-    for (var ai = 0; ai < tokens.length-1; ai++) {
-        var t = tokens[ai]+SPLIT_CHAR+tokens[ai+1];
-        if (encodeMap.hasOwnProperty(t)) {
-            bigramTmp.push(encodeMap[t]);
-            if (!smallDecodeMap.hasOwnProperty(encodeMap[t])) {
-                smallDecodeMap[encodeMap[t]] = t;
-            }
-            ai += 1;
-        } else bigramTmp.push(tokens[ai]);
-    }
-    tokens = bigramTmp;
-
-    //replace the unigrams
-    for (var ai = 0; ai < tokens.length; ai++) {
-        var t = tokens[ai];
-        if (encodeMap.hasOwnProperty(t) && encodeMap[t].length < t.length) {
-            tokens[ai] = encodeMap[t];
-            if (!smallDecodeMap.hasOwnProperty(encodeMap[t])) {
-                smallDecodeMap[encodeMap[t]] = t;
-            }
-        }
-    }
-*/
-    
     //assemble the output
-    var ret = JSON.stringify(smallDecodeMap)+'\n'+tokens.join(SPLIT_CHAR);
-    
+    var ret = JSON.stringify(smallDecMap)+'\n'+tokens.join(SPLIT_CHAR);
+
     //write it to a file
     fs.writeFile(OUT_FILE, ret, function (err) {
         if (err) return console.log(err);
@@ -214,6 +203,3 @@ function incrString(str) {
         return incrString(str.substring(0,l-1))+String.fromCharCode(low);
     }
 }
-
-
-
